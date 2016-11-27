c230033afac111f948830bc0bf4272e9b1bf89fc
olgalupuleac <olga_lupuleac@mail.ru>
2016-11-27 17:29:26 +0300

    let/where

--- Tree.hs
+++ Tree.hs
@@ -8,7 +8,7 @@ lookup :: Ord k => k -> BinaryTree k v -> Maybe v
 lookup x (Node k v left right) | x == k  = Just v
                                | x > k   = lookup x right
                                | x < k   = lookup x left
-lookup x _                               = Nothing
+lookup _ _                               = Nothing
 
 insert :: Ord k => k -> v -> BinaryTree k v -> BinaryTree k v
 insert x y Empty                          = Node x y Empty Empty 
@@ -17,14 +17,11 @@ insert x y (Node k v left right) | x > k  = Node k v left (insert x y right)
                                  | x == k = Node k y left right
 
 delete :: Ord k => k -> BinaryTree k v -> BinaryTree k v
-delete _ Empty                             = Empty
-delete x (Node k v Empty right) | x == k   = right
-                                | x > k    = Node k v Empty (delete x right)
-delete x (Node k v left Empty)  | x == k   = left
-                                | x < k    = Node k v (delete x left) Empty
-delete x (Node k v left right)  | x == k   = Node (fst (find_right(left))) (snd (find_right(left))) (delete (fst (find_right(left))) left) right
-                                | x > k    = Node k v left (delete x right)
-                                | x < k    = Node k v (delete x left) right
-                   
-find_right (Node k v _ Empty) = (k, v)
-find_right (Node k v _ right) = find_right right
+delete x (Node k v Empty right)  | x == k = right
+                                 | x > k  = Node k v Empty (delete x right)
+delete x (Node k v left right)   | x == k = let (k1, v1) = find_right left            
+                                            in Node k1 v1 (delete k1 left) right
+                                 | x > k  = Node k v left (delete x right)
+                                 | x < k  = Node k v (delete x left) right
+                                 where find_right (Node k v _ Empty) = (k, v)
+                                       find_right (Node _ _ _ right) = find_right right

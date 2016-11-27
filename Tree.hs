fab9c537077d1571167ab5f322aecf042ad7bd99
olgalupuleac <olga_lupuleac@mail.ru>
2016-11-27 09:31:20 +0300

    some code styles mistakes corrected

--- Tree.hs
+++ Tree.hs
@@ -17,14 +17,14 @@ insert x y (Node k v left right) | x > k  = Node k v left (insert x y right)
                                  | x == k = Node k y left right
 
 delete :: Ord k => k -> BinaryTree k v -> BinaryTree k v
-delete x Empty                            = Empty
-delete x (Node k v Empty right) | x == k  = right
-                                | x > k   = Node k v Empty (delete x right)
-delete x (Node k v left Empty)  | x == k  = left
-                                | x < k   = Node k v (delete x left) Empty
-delete x (Node k v left right)  | x == k  = Node (fst (find_right(left))) (snd (find_right(left))) (delete (fst (find_right(left))) left) right
-                                | x > k   = Node k v left (delete x right)
-                                | x < k   = Node k v (delete x left) right
+delete _ Empty                             = Empty
+delete x (Node k v Empty right) | x == k   = right
+                                | x > k    = Node k v Empty (delete x right)
+delete x (Node k v left Empty)  | x == k   = left
+                                | x < k    = Node k v (delete x left) Empty
+delete x (Node k v left right)  | x == k   = Node (fst (find_right(left))) (snd (find_right(left))) (delete (fst (find_right(left))) left) right
+                                | x > k    = Node k v left (delete x right)
+                                | x < k    = Node k v (delete x left) right
                    
 find_right (Node k v _ Empty) = (k, v)
 find_right (Node k v _ right) = find_right right
